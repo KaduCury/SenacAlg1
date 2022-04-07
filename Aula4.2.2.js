47dc54aad32c33d5d0b05e818276fd3cfad46381
Kadu Cury <carlosercury@gmail.com>
2022-04-07 17:25:35 -0300

    07/04 endafternoon

---
+++ Aula4.2.2.js
@@ -0,0 +1,11 @@
+var num
+
+num = prompt("Informe um número: ");
+
+if (num > 0){
+    alert("O número " + num + " é positivo.");
+} else if (num < 0){
+    alert("O número " + num + " é negativo.");
+} else {
+    alert("O número " + num + " é neutro.");
+}
---
+++ Aula5.3.js
@@ -0,0 +1,11 @@
+camaArrumada = prompt("A cama está arrumada? Responda com 's' para sim ou 'n' para não.");
+while (camaArrumada == 'n') {
+    alert("Você só pode sair se arrumar a cama!");
+    camaArrumada = prompt("Agora a cama está arrumada? Responda com 's' para sim ou 'n' para não.");    
+} alert("Agora que arrumou a cama você pode sair.");
+
+//if (camaArrumada == 's') {
+//    alert("Agora que arrumou a cama você pode sair.")
+//} else {
+//    alert("Você só pode sair se arrumar a cama!")
+//}
